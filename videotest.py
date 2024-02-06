b353a285e57614e7364ddb7c336e0a7d7fd029c0
PiMaster123 <63219690+PiMaster123@users.noreply.github.com>
2024-02-05 19:51:05 -0600

    Add files via upload

--- videotest.py
+++ videotest.py
@@ -16,6 +16,12 @@ LL = 5 # LINE_LENGTH
 options = apriltag.DetectorOptions(families="tag36h11")
 detector = apriltag.Detector(options)
 
+def plotText(image, center, color, text):
+    center = (int(center[0]) + 4, int(center[1]) - 4)
+    return cv.putText(image, str(text), center, cv.FONT_HERSHEY_SIMPLEX,
+                       1, color, 3)
+
+
 def plotPoint(image, center, color):
 	center = (int(center[0]), int(center[1]))
 	image = cv.line(image,
@@ -34,6 +40,7 @@ while(True):
     # Capture the video frame 
     # by frame 
     ret, frame = vid.read()
+    image=frame
     #print(ret, frame) 
     
     #frame = cv2.imread("/home/pi/testtag.jpg")
@@ -41,7 +48,7 @@ while(True):
     # Display the resulting frame 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     h, w = frame.shape[:2]
-    h,w = int(h/3.5),int(w/3.5)
+    h,w = int(h),int(w)
     r_h,r_w = h,w
     resize = cv.resize(gray, (w,h))
     detect = detector.detect(resize)
@@ -50,13 +57,39 @@ while(True):
       #print("Nothing")
       image = frame
     else:
+      #print(detect)
       for tag in detect:
-        #print(f"Tag: {detect.tag_id} Center: {detect.center}")
-        image = cv.cvtColor(resize, cv.COLOR_GRAY2BGR)
+        
         image = plotPoint(image, tag.center, CENTER_COLOR)
+        
+        # angle
+        c1 = tag.corners[0]
+        c2 = tag.corners[3] # side 1 is abs(p2-p1)
+        c3 = tag.corners[1] # side 2 is abs(p4-p3)
+        c4 = tag.corners[2]
+
+        s1 = abs(c2[1]-c1[1])
+        s2 = abs(c4[1]-c3[1])
+
+        val = s1/s2
+        print(tag.tag_id,-255.5*val+350.7)
+        
+        # corner marks
         for corner in tag.corners:
           image = plotPoint(image, corner, CORNER_COLOR)
-    
+        
+        # id
+        image = plotText(image, tag.center, CENTER_COLOR, str(tag.tag_id))
+          
+        if len(detect)>1:
+          c0 = detect[0].center
+          c1 = detect[1].center
+          c0, c1 = tuple([int(c0[0]),int(c0[1])]),tuple([int(c1[0]),int(c1[1])])
+          image = cv.line(image, c0, c1, (0, 255, 0), 3)
+          midp = (int(c0[0]+c1[0])/2,int(c0[1]+c1[1])/2)
+          image = plotPoint(image, midp, CENTER_COLOR)
+          #print(c0,c1)
+      
     cv.imshow('frame', image) 
       
     # the 'q' button is set as the 
@@ -71,15 +104,3 @@ while(True):
 vid.release() 
 # Destroy all the windows 
 cv.destroyAllWindows() 
-
-"""
-p1 = results[i].corners[0]
-p2 = results[i].corners[3] # side 1 is abs(p2-p1)
-p3 = results[i].corners[1] # side 2 is abs(p4-p3)
-p4 = results[i].corners[2]
-
-s1 = abs(p2[1]-p1[1])
-s2 = abs(p4[1]-p3[1])
-
-val = s1/s2
-"""
